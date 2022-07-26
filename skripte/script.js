e3ab779b273886aae7e0d96382c50df5fb3bc47a
Krkanoid <gospodaklosari@gmail.com>
2022-07-26 13:09:56 +0200

    #.

---
+++ skripte/script.js
@@ -0,0 +1,4 @@
+function downloadBtn() {
+    console.log(`%c Downloading...`, "color: blue; font-weight: bold;");
+    document.getElementById("db").innerHTML = "<i class='fa-solid fa-spinner fa-spin-pulse'> </i> Downloading...";
+}
